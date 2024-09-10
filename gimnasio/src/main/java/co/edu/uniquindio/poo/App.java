2c3e418c3d508e154abb1e18f43383d73c91a1c9
RaulYulbrayner <153127665+RaulYulbrayner@users.noreply.github.com>
2024-09-10 17:03:12 -0500

    update

--- gimnasio/src/main/java/co/edu/uniquindio/poo/App.java
+++ gimnasio/src/main/java/co/edu/uniquindio/poo/App.java
@@ -12,8 +12,9 @@ public class App {
         Gimnasio gimnasio = new Gimnasio("Bodytech");
 
         // Se crean los miembros
-        Miembro miembro1 = new Miembro("Carlos", 20, "Masculino", "12345", Membresia.MENSUAL);
-        Miembro miembro2 = new Miembro("Mario", 17, "Masculino", "1235", Membresia.ANUAL);
+        Miembro miembro1 = new Miembro("Carlosito", 20, "Masculino", "12345", Membresia.MENSUAL);
+        Miembro miembro2 = new Miembro("Maro", 17, "Masculino", "1235", Membresia.ANUAL);
+        Miembro miembro3 = new Miembro("Camilaa", 17, "Femenino", "19985", Membresia.ANUAL);
 
         // Se crean los entrenadores
         Entrenador entrenador = new Entrenador("Jorge", "Pesas", "1234", "jorge@gmail.com");
@@ -40,6 +41,15 @@ public class App {
         for (Miembro miembro : menores) {
             System.out.println(miembro);
         }
+
+        // Calcular edad promedio
+        gimnasio.mostrarMensaje("La edad promedio es: " + gimnasio.calcularPromedioEdad());
+
+        // Eliminar nombre por cantidad de vocales
+        gimnasio.eliminarNombreVocales();
+
+        // Moda
+        gimnasio.mostrarMensaje("La Moda de edades: " + gimnasio.modaEdades());
 
         System.out.println(gimnasio);
 
--- gimnasio/src/main/java/co/edu/uniquindio/poo/Gimnasio.java
+++ gimnasio/src/main/java/co/edu/uniquindio/poo/Gimnasio.java
@@ -98,20 +98,129 @@ public class Gimnasio {
     }
     // --------------------------------------------------------------
 
-
-
-    // --------------------Completar los metodos---------------------
-
-    /*
-     - calcularPromedioEdad()
-     - modaEdades()
-     - elimiarNombreVocales()
-     - determiarVocal()
+    /**
+     * Metodo para calcular la edad promedio
+     * 
+     * @return la edad
      */
+    public int calcularPromedioEdad() {
+        int sumatoria = 0;
+        for (Miembro miembro : miembros) {
+            sumatoria += miembro.getEdad();
+        }
+        sumatoria /= miembros.size();
+        return sumatoria;
+    }
+
+    public boolean determiarVocal(char letra) {
+        String vocales = "aeiouAEIOU";
+        if (vocales.indexOf(letra) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean determiarVocal2(char letra) {
+        boolean centinela = false;
+        if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+            centinela = true;
+        }
+        return centinela;
+    }
+
+    // ---------------------
+    /**
+     * Determinar vocal
+     * 
+     * @param nombre
+     * @return
+     */
+    private int determinarVocal(String nombre) {
+        int contador = 0;
+        char[] vocales = nombre.toLowerCase().toCharArray();
+        for (char vocal : vocales) {
+            if (vocal == 'a' || vocal == 'e' || vocal == 'i' || vocal == 'o' || vocal == 'u') {
+                contador += 1;
+            }
+        }
+        return contador;
+    }
+
+    /**
+     * eliminar nombre por vocales
+     */
+    public void eliminarNombreVocales2() {
+        for (Miembro miembro : miembros) {
+            if (determinarVocal(miembro.getNombre()) >= 3) {
+                miembros.remove(miembro);
+            }
+        }
+    }
+
+    public void eliminarNombreVocales() {
+        for (int i = miembros.size() - 1; i >= 0; i--) {
+            Miembro miembro = miembros.get(i);
+            int cantidadVocales = 0;
+            char[] chars = miembro.getNombre().toCharArray();
+            for (char letra : chars) {
+                if (determiarVocal2(letra)) {
+                    cantidadVocales += 1;
+                }
+            }
+            if (cantidadVocales >= 3) {
+                miembros.remove(i);
+            }
+        }
+    }
+
+    public int determinarVocal(Miembro miembro) {
+        int contador = 0;
+        String nombre = miembro.getNombre();
+        for (int i = 0; i < nombre.length(); i++) {
+            char c = nombre.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' ||
+                    c == 'O' | c == 'U') {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public void EliminarNombreVocales() {
+        for (Miembro miembro : miembros) {
+            if (determinarVocal(miembro) >= 3) {
+                miembros.remove(miembro);
+            }
+        }
+    }
+
+    // --------------------------------------------------------------
+    public int modaEdades() {
+        int moda = -1;
+        int maxFrecuencia = 0;
+
+        for (int i = 0; i < miembros.size(); i++) {
+            int edadActual = miembros.get(i).getEdad();
+            int frecuencia = 0;
+
+            // Contar cuántas veces aparece la edad actual
+            for (int j = 0; j < miembros.size(); j++) {
+                if (miembros.get(j).getEdad() == edadActual) {
+                    frecuencia++;
+                }
+            }
+
+            // Actualizar la moda si la frecuencia actual es mayor que la anterior
+            if (frecuencia > maxFrecuencia) {
+                maxFrecuencia = frecuencia;
+                moda = edadActual;
+            }
+        }
+
+        return moda;
+    }
 
     // --------------------Completar---------------------------------
-
-
 
     public String getNombre() {
         return nombre;
